831f8b8595543cc128d5df463d3a82999d8755d7
Dev-Elie <eochieng9448@gmail.com>
2021-07-05 20:36:55 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,64 +1,51 @@
 import Card from './Components/Card'
-import Lists from './Components/Lists'
 
 import faker from 'faker'
 import { useState } from 'react'
 
 
 function App() {
-  const buttons = (
-    <div>
-        <a href="www.mut.ac.ke" className="btn btn-primary me-1">Accept</a>
-        <a href="www.mut.ac.ke" className="btn btn-danger">Reject</a>
-    </div>
-    )
-
   const[name,setName]=useState('Ondiek Elijah')
   const [showCard,setShowCard] = useState(true)
+  const [cards,setCards] = useState([
+      {
+        name:'Jose Musimi',
+        title:'Managing Director',
+        avatar:'https://cdn.fakercloud.com/avatars/albertaugustin_128.jpg'
+      },
+      {
+        name:'James Murio',
+        title:'Software Engineer',
+        avatar:'https://cdn.fakercloud.com/avatars/albertaugustin_128.jpg'
+      },
+      {
+        name:'Omondi Max',
+        title:'Machine Learning Engineer',
+        avatar:'https://cdn.fakercloud.com/avatars/albertaugustin_128.jpg'
+      }
+  
+  ])
 
-  const updateName = (name)=>{
-    setName(name)
-  }
-
-  const updateInput =(event)=>{
-    setName(event.target.value)
-  }
 
   const toggleShowCard = ()=>{
     setShowCard(!showCard)
   }
 
-  const cardMarkdown =  showCard &&  <Card name={name} 
-      title='Dynamic Metrics Agent'
-      image='https://cdn.fakercloud.com/avatars/albertaugustin_128.jpg'
-      onChangeName={()=>updateName('Ochieng Ondiek')}
-      onChangeInput={updateInput}
-      >
-      {buttons}
-      </Card>
+  const cardMarkdown =  showCard &&  (
+        cards.map((card)=>
+        <Card name={card.name} 
+        title={card.title}
+        image={card.avatar}
+        />
+  ))
 
   return (
     <div className="App">
       <div className='text-center m-2'>
-      <button onClick={()=>updateName('Ondiek Elijah')} className="btn btn-success">Update</button>
       <button onClick={toggleShowCard} className="btn btn-success">Toggle</button>
 
       </div>
-
       {cardMarkdown}
-
-      {/*You can either use this or assign it to a variable to make the code cleaner*/} 
-      {/*      { showCard &&     <Card name={name} 
-            title='Dynamic Metrics Agent'
-            image='https://cdn.fakercloud.com/avatars/albertaugustin_128.jpg'
-            onChangeName={()=>updateName('Ochieng Ondiek')}
-            onChangeInput={updateInput}
-            >
-            {buttons}
-            </Card>}
-      */}
-      <h3 className='text-center'>List</h3>
-      <Lists />
     </div>
   );
 }
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,15 +1,13 @@
-const Card = ({ title,name,image,children,onChangeName,onChangeInput}) => {
+const Card = ({ title,name,image}) => {
     return (
-  		<div className="card">
-  		<img src={image} className="card-img-top" alt="..."/>
-  		<div className="card-body">
-  		  <h5 className="card-title"> { name } </h5>
-          <input type='text' onChange={onChangeInput} value={name} />
-          <button className='btn btn-success' onClick={onChangeName} >Change Name</button>
-  		  <p className="card-text"> { title } </p>
-          {children }
-  		</div>
-  		</div>
+        <div className="card text-center">
+        <img src={image} className="card-img-top" alt="..."/>
+        <div className="card-body">
+          <h5 className="card-title"> { name } </h5>
+          <p className="card-text"> { title } </p>
+        <button className='btn btn-danger'>Delete</button>
+        </div>
+        </div>
     )
 }
 
